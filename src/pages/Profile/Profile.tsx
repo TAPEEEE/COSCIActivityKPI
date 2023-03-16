8360d9c901de38d0c2bb26466435306f9333c41c
Nattapon Suwanno <nattapon.tape@g.swu.ac.th>
2023-03-16 23:15:53 +0700

    KPI Pages Add

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+type ProfileProps = {
+  //
+};
+
+const Profile: React.FC<any> = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
---
+++ src/pages/TeacherKPIDetail/TeacherKPIDetail.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+type TeacherKPIDetailProps = {
+  //
+};
+
+const TeacherKPIDetail: React.FC<any> = () => {
+  return <div>TeacherKPIDetail</div>;
+};
+
+export default TeacherKPIDetail;
